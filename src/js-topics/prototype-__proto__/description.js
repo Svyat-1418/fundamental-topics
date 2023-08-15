b3db51455109adbcf9779b43c7bea159a7906ea6
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-08-15 23:24:36 +0300

    add description for prototype / __proto__

---
+++ src/js-topics/prototype-__proto__/description.js
@@ -0,0 +1,63 @@
+// prototype, __proto__
+
+//1. Прототип (prototype) - объект, на который указывает скрытая ссылка
+//   [[Prototype]] в другом объекте
+//2. X.__proto__ ----> XConstructor.prototype
+//3. functionName.prototype
+
+// ===========================================================
+
+//1. .prototype есть только в функциях и классах (НО НЕ в стрелочных функциях)
+//2. __proto__ есть у каждого объекта
+
+// ===========================================================
+
+
+// ==============================================================
+function Person(name) {
+    this.name = name
+}
+const john = new Person("John")
+
+// fConstructor === person
+// Person.prototype --->
+//                         ----> { constructor: fConstructor } <-- default
+// (true).__proto__ --->
+// fConstructor === Boolean
+
+// ===========================================================
+
+function userConstructor(age) {
+
+     const userPrototype = {
+         constructor: userConstructor
+     }
+
+    const newUser = {}
+
+    newUser.__proto__ = userPrototype
+
+    newUser.age = age
+
+    return newUser
+
+}
+
+const harry = userConstructor(32)
+console.log(harry)
+
+function User(age) {
+    this.age = age
+}
+const ron = new User(33)
+console.log(ron)
+// с "new" под капюшоном выполняется следующее:
+// this.__proto__ = { constructor: userConstructor }
+
+// ===========================================================
+
+console.log(Object.__proto__ === Function.prototype)
+
+console.log(Object.prototype.__proto__ === null)
+
+console.log((1).__proto__ === Number.prototype)
